b76732ae6ddc3ffdbd7b978ce21ad099a3f34d27
Krisadakorn Chaiwong <>
2021-01-19 14:52:25 +0700

    Add action text feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :welcome
+  resources :posts
   root 'welcome#index' 
   get '/action_text', to: 'action_text#index', as: 'action_text'
   get '/chat_app', to: 'chat_app#index', as: 'chat_app'
   get '/paperclip', to: 'paperclip#index', as: 'paperclip'
-  get '/landingpage', to:'landing_page#index', as: 'landingpage'
+  get '/landingpage', to: 'landing_page#index', as: 'landingpage'
+  get '/posts_index', to: 'posts#index', as: 'posts_index'
+
 end
