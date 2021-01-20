9f27d1f991b5b0426d2c56d086fe5a2f1e81df92
natapapon <natapapon.r@gmail.com>
2021-01-20 14:54:30 +0700

    fix destroy room and add back with post path

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   end
   resources :welcome
   resources :posts
+
   root 'welcome#index' 
   get '/action_text', to: 'action_text#index', as: 'action_text'
   get '/chat_app', to: 'chat_app#index', as: 'chat_app'
   get '/paperclip', to: 'paperclip#index', as: 'paperclip'
   get '/landingpage', to: 'landing_page#index', as: 'landingpage'
   get '/posts_index', to: 'posts#index', as: 'posts_index'
-
+  post '/index', to: 'welcome#index', as: 'back_to_index'
+  
 end
