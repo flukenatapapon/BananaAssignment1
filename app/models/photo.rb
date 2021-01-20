2aaeceec7b9762ddd893d32d6a5c6fcb6514e72b
natapapon <natapapon.r@gmail.com>
2021-01-20 14:03:14 +0700

    no message

---
+++ app/models/photo.rb
@@ -0,0 +1,8 @@
+class Photo < ApplicationRecord
+    #Mounts paperclip image
+    has_attached_file :image
+    #This validates the type of file uploaded. According to this, only images are allowed.
+    validates_attachment :image, presence: true,
+    content_type: { content_type: ["image/jpeg", 'image/png' ] },
+    size: { in: 0..10.kilobytes }
+end
