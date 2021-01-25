8046baac491decc2395c275a5557e7b8ed86deea
natapapon <natapapon.r@gmail.com>
2021-01-25 14:05:16 +0700

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.assets.initialize_on_precompile = false
-  warnings: true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
