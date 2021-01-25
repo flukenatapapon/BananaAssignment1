d78f68f7eedf0faa597deede96c634f7f2587949
natapapon <natapapon.r@gmail.com>
2021-01-25 13:53:38 +0700

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Assignment1Banana
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     # config/application.rb
-config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
