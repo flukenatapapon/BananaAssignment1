ecb66c4283e18ff5a2bbbe762ee7516bfdd1571c
natapapon <natapapon.r@gmail.com>
2021-01-25 16:15:12 +0700

    no message

--- app/assets/javascripts/packs/application.js
+++ app/assets/javascripts/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
 
 import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
@@ -23,12 +22,3 @@ require("@rails/actiontext")
 // require("channels")
 // require("packs/custom")
 // require("packs/application.js")
-require("jquery");
-require("bootstrap")
-import "../stylesheets/application";
-document.addEventListener("turbolinks:load", function() {
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="popover"]').popover()
-    })
-})
